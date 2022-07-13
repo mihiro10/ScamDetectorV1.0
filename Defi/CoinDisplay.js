8cdcb679bce519bea0649fc95a08a75b419311b6
mihiro10 <mihirosuzuki@utexas.edu>
2022-07-13 16:46:56 -0500

    json stuff

---
+++ Defi/CoinDisplay.js
@@ -0,0 +1,27 @@
+app.component('coin-display',{
+    template:
+    /*html*/
+    `<div class="product-display">
+        <div class="product-container">
+             <div class="product-image">
+                <img v-bind:src="image">
+             </div>
+        <div class="product-info">
+            <h1>{{coin}}</h1>
+        <div>
+        <ul>
+          <div
+          class="color-circle"
+          :style="{ backgroundColor: 'blue' }">
+
+        </ul>
+      </div>
+    </div>
+  </div>`,
+  data() {
+    return {
+        coin: 'Bitcoin',
+        image: './assets/images/Bitcoin.png',
+    }
+}
+})
---
+++ Intro/intro.js
@@ -0,0 +1,20 @@
+app.component('intro-display',{
+    template:
+    /*html*/
+    `<center><div class="intro-image">
+        <img v-bind:src="image"> <!-- center and fill box-->
+        <h1>Crypto Scam Detector<h1>
+        <h3>Alerting you of the latest security risks in Web 3.0<h3>
+        
+       <p> CSD uses the latest information from the internet to inform you of potential and confirmed security breaches detected. We aim to minimize the noise of Web 3.0 by providing only essential information. 
+        
+        Find out if there has been a detection of a security breach in the past 6 months relating to your assets in Web 3.0
+         
+        <p>
+     </div></center>`,
+  data() {
+    return {
+        image: './assets/images/spotlight.jpg',
+    }
+}
+})
