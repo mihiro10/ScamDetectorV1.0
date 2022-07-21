80ae34a058fd0a895a2e97417a5cc9900b9881b2
mihiro10 <mihirosuzuki@utexas.edu>
2022-07-21 16:02:33 -0500

    defi protocol page edits

--- Defi/defiIntro.js
+++ Defi/defiIntro.js
@@ -4,8 +4,14 @@ app.component('defi-intro',{
     `<center><div class="intro-image">
         <img v-bind:src="image" style="width:100%, padding:0, margin:0"> <!-- center and fill box-->
         <h1>DeFi Protocols<h1> <!-- change font to Iceberg -->
-        <p style = "font-family:georgia,garamond,serif;font-size:16px;font-style:italic;">The DeFi protocols with security breaches identified in the past 12 months are listed below</p>
-     </div></center>`,
+        <div class="white-line"></div>
+        <p>The DeFi protocols with security breaches identified in the past 12 months are listed below</p>
+        <div class="white-line"></div>
+        <p>TICKER SYMBOL &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;NAME &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;CHAIN &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; SECURITY BREACH: Y/N?</p>
+        <div class="s-white-line"></div>
+    </div></center>`,
+     
+
   data() {
     return {
         image: './assets/images/Defi_pic.jpg',
